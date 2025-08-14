bd954f24f57f0ccfcfce6ef24f56319cb24e31ee
helal-shohag <shohag.2896@gmail.com>
2025-08-15 01:47:16 +0600

    ok

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    user = models.ForeignKey(User, related_name='product',on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    brand = models.CharField(max_length=100, blank=True) 
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+    stock = models.PositiveIntegerField(default=0) 
+    image = models.ImageField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    _id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.name
+   
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name='reviews',on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=3,decimal_places=2)
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    _id = models.AutoField(primary_key=True)
+
+
+    def __str__(self):
+        return f"{self.user.username}- {self.product.name} - {self.rating}"
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=20, unique=True)
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)    
+    payment_method = models.CharField(max_length=50, choices=[('Credit Card', 'Credit Card'), ('PayPal', 'PayPal'), ('Bank Transfer', 'Bank Transfer')])
+    shipping_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)  
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Cancelled', 'Cancelled')], default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    _id = models.AutoField(primary_key=True)
